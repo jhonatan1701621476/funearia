2064fe2a9b308a9673b5a07fc973e046e08ed0ec
Daniel Cardona <carlos.cardona28426@ucaldas.edu.co>
2024-04-26 08:34:28 -0500

    Pagina Maestra Materialize

---
+++ src/app/publico/errores/ruta-no-encontrada/ruta-no-encontrada.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ruta-no-encontrada',
+  templateUrl: './ruta-no-encontrada.component.html',
+  styleUrl: './ruta-no-encontrada.component.css'
+})
+export class RutaNoEncontradaComponent {
+
+}
