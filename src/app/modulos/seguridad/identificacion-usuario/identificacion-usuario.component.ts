1e886e8e5688ce4d8ceeae061d2e9c00d0991930
Mamejiap <mateo.mejia32303@ucaldas.edu.co>
2024-04-29 00:20:08 -0500

    Se ha construido el formulario de identificación, se ha validado el formulario, y se han mostrado mensajes especificos en su respectivo campo

--- src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
+++ src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-identificacion-usuario',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './identificacion-usuario.component.css'
 })
 export class IdentificacionUsuarioComponent {
+  fGroup: FormGroup = new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.ConstruirFormulario(); // Al iniciar el componente construye el formulario
+  }
+
+  ConstruirFormulario() {
+    this.fGroup = this.fb.group({
+      usuario: [Validators.required, Validators.email], // Validar que sea un email, es un control del formulario
+      clave: [Validators.required] // Validar que no sea vacio, es un control del formulario
+    });
+  }
+
+  IdentificarUsuario() {
+    if (this.fGroup.invalid){
+      alert("Datos Incompletos..")
+    } else {
+      alert ("Identificar Usuario..")
+    }
+  }
+
+  get obtenerFormGroup(){
+    return this.fGroup.controls; // Obtener los controles del formulario
+  }
 
 }
